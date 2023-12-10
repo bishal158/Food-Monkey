8f17f60c0c57c31dd6b73bd056ef072aafbb7727
Mafuj Ahmed Bishal <afnanmafuj22@gmail.com>
2023-12-10 10:26:42 +0600

    Cities location complete

--- client/src/pages/components/Cities.jsx
+++ client/src/pages/components/Cities.jsx
@@ -1,21 +1,73 @@
 import "./styles/Cities.css";
-function Cities(options) {
-  const cities = ["Dhaka", "Mymensingh", "Chittagong", "Sylhet", "Rangpur"];
+import Dhaka from "../../assets/images/Dhaka.jpg";
+import Chittagong from "../../assets/images/Chittagong.jpg";
+import Sylhet from "../../assets/images/Sylhet.jpg";
+import Rajshahi from "../../assets/images/Rajshahi.webp";
+import Rangpur from "../../assets/images/Rangpur.jpg";
+import Mymensingh from "../../assets/images/Mymensingh.jpg";
+import Chilli from '../../assets/Chilli.png'
+import {Link} from "react-router-dom";
+
+const Cities = () => {
+  const cities = [
+    {
+      name: "Dhaka",
+      cover: Dhaka,
+    },
+    {
+      name: "Mymensingh",
+      cover: Mymensingh,
+    },
+    {
+      name: "Rangpur",
+      cover: Rangpur,
+    },
+    {
+      name: "Rajshahi",
+      cover: Rajshahi,
+    },
+    {
+      name: "Chittagong",
+      cover: Chittagong,
+    },
+    {
+      name: "Sylhet",
+      cover: Sylhet,
+    },
+    {
+      name: "Sylhet",
+      cover: Sylhet,
+    },
+    {
+      name: "Sylhet",
+      cover: Sylhet,
+    },
+    {
+      name: "Sylhet",
+      cover: Sylhet,
+    },
+  ];
 
   return (
-    <>
-      <div className={"container-fluid cities-container"}>
-        <h2>Find us in these cities and many more!</h2>
-        <div className={"cities"}>
-          {cities.map((city) => (
-            <div className="card city" key={city}>
-              <div className="card-body">{city}</div>
-            </div>
-          ))}
+      <>
+        <div className={"container-fluid cities-container"}>
+          <h2>Find us in these cities and many more!</h2>
+          <div className={"cities"}>
+            {cities.map((city,index) => (
+                <Link className="card city" key={index} to={'/login'}>
+                  <img src={city.cover} className="card-img-top" alt="..."/>
+                  <div className="card-body city-name">
+                    <p>{city.name}</p>
+                  </div>
+                </Link>
+            ))}
+          </div>
         </div>
-      </div>
-    </>
+        {/*<div className={'bottom-chilli'}>*/}
+        {/*  <img src={Chilli} alt={'...'}/>*/}
+        {/*</div>*/}
+      </>
   );
-}
+};
 
 export default Cities;
